01ae51638c713bd2b65ca7ab39820fff2dbbc01d
BryanGC96 <2014104@icum.edu.mx>
2024-05-27 13:05:45 -0600

    Added 'icons' to represent the footer links

--- React_Portfolio/src/components/Footer.jsx
+++ React_Portfolio/src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LayersIcon from '@mui/icons-material/Layers';
 
 const Footer = () => {
     return (
         <footer>
             <p>
-                <a href='https://github.com/BryanGC96'>GitHub Profile</a> | 
-                <a href='https://stackoverflow.com/users/25260618/bryan-g-c'> Stack Overflow</a>
+                <a href='https://github.com/BryanGC96'><GitHubIcon sx={{ fontSize: 60}}></GitHubIcon></a>   | 
+                <a href='https://stackoverflow.com/users/25260618/bryan-g-c'><LayersIcon sx={{ fontSize: 60}}></LayersIcon>  </a>
             </p>
         </footer>
     );
